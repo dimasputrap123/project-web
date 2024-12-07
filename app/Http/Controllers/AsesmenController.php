5a311409caba1bb139cc0cef558279272b75f952
dimas <dimasputrap@outlook.com>
2024-12-07 19:21:44 +0700

    progres

--- app/Http/Controllers/AsesmenController.php
+++ app/Http/Controllers/AsesmenController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class AsesmenController extends Controller
 {
-    //
+    function add_master_bantuan(Request $request)  {
+        // untuk menambah jenis master bantuan
+    }
+
+    function add_master_kategori()  {
+        
+    }
+
+    function add_master_asesmen()  {
+        
+    }
+
+    function add_kpm() {
+        
+    }
 }
