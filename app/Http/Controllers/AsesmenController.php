26b89f88585928d8073da02a083134b6af5f534c
S - Crocophim <iqbalm0702@gmail.com>
2024-12-12 20:44:33 +0700

    Update AsesmenController.php
    Update :
    function add_master_kategori
    function add_master_asesmen
    function add_kpm
    function add_master_bantuan

--- app/Http/Controllers/AsesmenController.php
+++ app/Http/Controllers/AsesmenController.php
@@ -17,14 +17,80 @@ class AsesmenController extends Controller
 
     function add_master_bantuan(Request $request)
     {
-        // untuk menambah jenis master bantuan
+        $validator = Validator::make($request->all(), [
+            'id' => 'required|string|max:30',
+            'bantuan' => 'required|string|max:255|',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $master_bantuan = MasterBantuan::create([
+            'id' => $request->id,
+            'bantuan' => $request->bantuan,
+        ]);
+
     }
 
-    function add_master_kategori() {}
+    function add_master_kategori(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'id' => 'required|string|max:30',
+            'kategori' => 'required|string|max:255|',
+        ]);
 
-    function add_master_asesmen() {}
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
 
-    function add_kpm() {}
+        $master_kategori = MasterKategori::create([
+            'id' => $request->id,
+            'kategori' => $request->kategori,
+        ]);
+
+    }
+
+    function add_master_asesmen(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'id' => 'required|string|max:30',
+            'pertanyaan' => 'required|string|max:255',
+            'status' => 'required|string|max:30|',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $master_asesmen = MasterAsesmen::create([
+            'id' => $request->id,
+            'pertanyaan' => $request->pertanyaan,
+            'status' => $request->status,
+        ]);
+
+    }
+
+    function add_kpm(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'id' => 'required|string|max:30',
+            'nik' => 'required|string|max:255',
+            'nama' => 'required|string|max:255|',
+            'alamat' => 'required|string|max:255|',
+            'status' => 'required|string|max:30|',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $kpm = Kpm::create([
+            'id' => $request->id,
+            'nik' => $request->nik,
+            'nama' => $request->nama,
+            'alamat' => $request->alamat,
+            'status' => $request->status,
+        ]);
+
+    }
 
     function tambahSurvey(Request $request)
     {
