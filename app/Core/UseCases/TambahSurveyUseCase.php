c71bef88bb02820b51a09e43fd160484757260a5
dimas <dimasputrap@outlook.com>
2025-01-03 05:35:26 +0700

    progres

--- app/Core/UseCases/TambahSurveyUseCase.php
+++ app/Core/UseCases/TambahSurveyUseCase.php
@@ -5,6 +5,7 @@ namespace App\Core\UseCases;
 use App\Core\Entities\HasilSurvey;
 use App\Core\Entities\Survey;
 use App\Core\Interfaces\IHasilSurveyRepository;
+use App\Core\Interfaces\IKpmRepository;
 use App\Core\Interfaces\ISurveyRepository;
 use App\Http\Request\TambahSurveyRequest;
 use Illuminate\Support\Facades\DB;
@@ -13,11 +14,13 @@ class TambahSurveyUseCase
 {
     private ISurveyRepository $surveyRepository;
     private IHasilSurveyRepository $hasilSurveyRepository;
+    private IKpmRepository $kpmRepository;
 
-    public function __construct(ISurveyRepository $survey, IHasilSurveyRepository $hasil)
+    public function __construct(ISurveyRepository $survey, IHasilSurveyRepository $hasil, IKpmRepository $kpm)
     {
         $this->surveyRepository = $survey;
         $this->hasilSurveyRepository = $hasil;
+        $this->kpmRepository = $kpm;
     }
 
     function execute(TambahSurveyRequest $request, int $id_user)
@@ -33,6 +36,7 @@ class TambahSurveyUseCase
                 array_push($survey_array, new Survey($id_hasil, $request->id_kpm, $item->id_pertanyaan, $item->jawaban));
             }
             $this->surveyRepository->save($survey_array);
+            $this->kpmRepository->updateStatusById($request->id_kpm, 1, $id_user, $now);
             DB::commit();
         } catch (\Throwable $th) {
             DB::rollBack();
@@ -40,7 +44,8 @@ class TambahSurveyUseCase
         }
     }
 
-    function findMax($a, $b, $c) {
+    function findMax($a, $b, $c)
+    {
         return max($a, $b, $c);
     }
 }
